3795023d65424b368e6e847e84862c78228413f1
Mike ☕ <topunixguy@gmail.com>
2026-06-16 15:54:03 -0500

    Update setup.py for version 0.2.0 and dependencies
    Updated version to 0.2.0 and modified dependencies. Changed README file format from .rst to .md and added support for sync and async views.

--- setup.py
+++ setup.py
@@ -5,31 +5,44 @@ from setuptools import find_packages, setup
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 # get README
-with open('README.rst') as f:
+with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
     name='django-explain-errors',
-    version='0.1',
-    packages=find_packages(),
-    description='Django middleware that captures errors and exceptions, sends them to OpenAI for a detailed explanation, and prints the explanation to stdout when debug mode is enabled',
-    long_description_content_type="text/markdown",
-    long_description = long_description,
-    install_requires=['Django>=2'],
+    version='0.2.0',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    description='Django middleware that captures errors and exceptions, sends them to OpenAI for a detailed explanation, and prints the explanation to stdout when debug mode is enabled. Supports both sync and async views.',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    python_requires='>=3.9',
+    install_requires=[
+        'Django>=4.2',
+        'openai>=1.0',
+        'python-dotenv>=1.0',
+        'asgiref>=3.6',
+    ],
     url='https://github.com/topunix/django-explain-errors',
     author='topunix',
     author_email='topunixguy@gmail.com',
     license='MIT',
     classifiers=[
+        'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Framework :: Django',
-        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 4.2',
+        'Framework :: Django :: 5.0',
+        'Framework :: Django :: 5.1',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
         'Topic :: Internet :: WWW/HTTP',
     ],
 )
